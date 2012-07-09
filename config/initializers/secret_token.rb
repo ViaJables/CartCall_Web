feecd87da50c1ac3ec93202c8b29d2f9f63e2396
John Brunsfeld <john@rewardsforce.com>
2012-07-09 15:53:43 -0700

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CartCallWeb::Application.config.secret_token = '76c2a5a5aa2786b9a01ab8549a1327bfb48818e4e119175923e14be98f866f4194ef85b34424897377a6df2719b50e6c41d6aa2dce918ce935f00c7f81cc6495'
